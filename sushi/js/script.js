545531f448a1dd01dcd34c57588fa95798f59a08
Sakiuzzamankhan <jjohnykhan@outlook.com>
2019-02-10 01:07:18 +0600

    first commit

---
+++ sushi/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	
+	
+	$(#header, .info).ripples({
+  dropRadius: 25,
+  perturbance:0.6,
+  ...
+});	
+// magnefic
+$('.parent-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{
+	  enabled:true
+  // other options
+});
+
+});
